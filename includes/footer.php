2849993585edd19a619bc6d3c79767c61292a2b3
Shahadat Nayeem <shahadatnayeem23@gmail.com>
2021-05-01 04:34:57 +0600

    added identation raining & syntax of html snippets

--- includes/footer.php
+++ includes/footer.php
@@ -1,50 +1,52 @@
 <footer class="footer">
-<div class="footer-container">
-<div class="footer-row">
-<div class="footer-col">
-<h4>Contact</h4>
-<ul>
-<li>
-<a href="#"><i class="fa fa-phone"></i> +880 1621720057</a>
-<a href="#"><i class="fa fa-envelope"></i> shahadatnayeem@gmail.com</a>
-</li>
-</ul>
-</div>
+			<div class="footer-container">
+				<div class="footer-row">
+					<div class="footer-col">
+						<h4>Contact</h4>
+						<ul>
+							<li>
+								<a href="#"><i class="fa fa-phone"></i> +880 1621720057</a>
+								<a href="#"
+									><i class="fa fa-envelope"></i> shahadatnayeem@gmail.com</a
+								>
+							</li>
+						</ul>
+					</div>
 
-<div class="footer-col">
-<h4>Address</h4>
-<ul>
-<li>
-<a href="#">Block #F, Road #06, House #12, Mirpur-1, Dhaka-1216.</a>
-</li>
-</ul>
-</div>
+					<div class="footer-col">
+						<h4>Address</h4>
+						<ul>
+							<li>
+								<a href="#"
+									>Block #F, Road #06, House #12, Mirpur-1, Dhaka-1216.</a
+								>
+							</li>
+						</ul>
+					</div>
 
-<div class="footer-col">
-<h4>Google Map Location</h4>
-<ul>
-<li>
-<a href="#">Google API</a>
-</li>
-</ul>
-</div>
+					<div class="footer-col">
+						<h4>Google Map Location</h4>
+						<ul>
+							<li>
+								<a href="#">Google API</a>
+							</li>
+						</ul>
+					</div>
 
-<div class="footer-col">
-<h4>Find us on</h4>
+					<div class="footer-col">
+						<h4>Find us on</h4>
 
-<div class="social-links">
-<a href="#"><i class="fab fa-facebook-f"></i></a>
-<a href="#"><i class="fab fa-instagram"></i></a>
-</div>
-
-</div>
-
-</div>
-<div class="footer-bottom">
-    &copy; Shahadat Nayeem | Arnob Mallick | Hasibul Hassan Sarkar
-</div>
-</div>
-</footer>
+						<div class="social-links">
+							<a href="#"><i class="fab fa-facebook-f"></i></a>
+							<a href="#"><i class="fab fa-instagram"></i></a>
+						</div>
+					</div>
+				</div>
+				<div class="footer-bottom">
+					&copy; Shahadat Nayeem | Arnob Mallick | Hasibul Hassan Sarkar
+				</div>
+			</div>
+		</footer>
 
 
  <script type="text/javascript" src="js/main.js"></script>
