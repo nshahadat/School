50b9e901b938bab53396660fcdb1bb3d8c400a94
Shahadat Nayeem <shahadatnayeem23@gmail.com>
2021-05-01 03:24:22 +0600

    added footer

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Habiba International School</title>
-    <link rel="icon" href="assests/logo.png">
+    <link rel="icon" href="assests/H.png">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -3,8 +3,9 @@ include "includes/header.php";
 include "includes/navbar.php";
 ?>
 
+<div class="container">
 <h2><marquee direction="left">Habiba International School</marquee></h2>
-
+</div>
 <?php
 include "includes/footer.php";
 ?>
