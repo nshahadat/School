ce1399545ebf36d1c77714726baa0590620250c9
Shahadat Nayeem <shahadatnayeem23@gmail.com>
2021-05-02 01:05:10 +0600

    grid layout with demo content

--- index.php
+++ index.php
@@ -3,9 +3,43 @@ include "includes/header.php";
 include "includes/navbar.php";
 ?>
 
-<div class="container">
-<h2><marquee direction="left">Habiba International School</marquee></h2>
+
+    <div class="wrapper">
+	<div class="image-slider">
+		<div class="wrap">
+			<div id="arrow-left" class="arrow"></div>
+			<div id="slider">
+				<div class="slide slide1">
+					<div class="slide-content">
+						<span>Vibing</span>
+					</div>
+				</div>
+				<div class="slide slide2">
+					<div class="slide-content">
+						<span>Get On</span>
+					</div>
+				</div>
+				<div class="slide slide3">
+					<div class="slide-content">
+						<span>Underwaved</span>
+					</div>
+				</div>
+				<div class="slide slide4">
+					<div class="slide-content">
+						<span>Fast Saga</span>
+					</div>
+				</div>
+			</div>
+			<div id="arrow-right" class="arrow"></div>
+		</div>
+	</div>
+	<div class="school-name-slogan">
+		<marquee>Habiba International School</marquee>
+	</div>
+	<div class="notice-box">notice box</div>
+	<div class="youtube">youtube video</div>
 </div>
+
 <?php
 include "includes/footer.php";
 ?>
---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Image SLider START
+let sliderImages = document.querySelectorAll(".slide"),
+	arrowRight = document.querySelector("#arrow-right"),
+	arrowLeft = document.querySelector("#arrow-left"),
+	current = 0;
+
+function reset() {
+	for (let i = 0; i < sliderImages.length; i++) {
+		sliderImages[i].style.display = "none";
+	}
+}
+
+function startSlide() {
+	reset();
+	sliderImages[0].style.display = "block";
+}
+
+function slideLeft() {
+	reset();
+	sliderImages[current - 1].style.display = "block";
+	current--;
+}
+
+function slideRight() {
+	reset();
+	sliderImages[current + 1].style.display = "block";
+	current++;
+}
+arrowLeft.addEventListener("click", function () {
+	if (current === 0) {
+		current = sliderImages.length;
+	}
+	slideLeft();
+});
+arrowRight.addEventListener("click", function () {
+	if (current === sliderImages.length - 1) {
+		current = -1;
+	}
+	slideRight();
+});
+startSlide();
+// Image Slider END
