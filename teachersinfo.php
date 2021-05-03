8eb78538b3a2f4bae93fc7e2f72155c0f6753804
Shahadat Nayeem <shahadatnayeem23@gmail.com>
2021-05-03 22:29:28 +0600

    teachers layout changed

--- teachersinfo.php
+++ teachersinfo.php
@@ -3,6 +3,11 @@ include "includes/header.php";
 include "includes/navbar.php";
 ?>
 
+<div class="teachers-container">
+    <div class="col t-image"> <img src="assests/shahadat.jpg"></div>
+    <div class="col t-image"><img src="assests/safran.jpg"></div>
+</div>
+
 <?php
 include "includes/footer.php";
 ?>
