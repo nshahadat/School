d5f60cfbe853ed9b13ee51a1fea4d813fdd2271a
Shahadat Nayeem <shahadatnayeem23@gmail.com>
2021-05-04 00:11:56 +0600

    minor changes

--- teachersinfo.php
+++ teachersinfo.php
@@ -4,22 +4,21 @@ include "includes/navbar.php";
 ?>
 <h6 class=t-header1>Math teachers</h6>
 <div class="teachers-container">
-   
-<ul class="nav-links">
-    <div class="col t-image"> <img src="assests/shahadat.jpg"> 
-    <li class="nav-item">
-        <p class = "teachersname">Shahadat Nayeem</p>
-    <div class="t-info1">
-           <ul >
-           <li class="nav-item">
-            <p class="t-email">shahadatnayeem23@gmail.com</a>
-            </li><li class="nav-item">
-            <p class="t-eduquali">Nasa's Most Young and Bright Scientist</a>
-            </li><li class="nav-item">
-            <p class="t-subjects">Can Teach Anything if He is willing</a>
-            </li>
-          </ul>
-         </div>
+    <ul class="nav-links">
+        <div class="col t-image"> <img src="assests/shahadat.jpg"> 
+        <li class="nav-item">
+            <p class = "teachersname">Shahadat Nayeem</p>
+        <div class="t-info1">
+            <ul >
+            <li class="nav-item">
+                <p class="t-email">shahadatnayeem23@gmail.com</a>
+                </li><li class="nav-item">
+                <p class="t-eduquali">Nasa's Most Young and Bright Scientist</a>
+                </li><li class="nav-item">
+                <p class="t-subjects">Can Teach Anything if He is willing</a>
+                </li>
+            </ul>
+            </div>
 </div>
     
 
@@ -27,16 +26,16 @@ include "includes/navbar.php";
     <li class="nav-item">
     <p class = "teachersname">Hasibul Hassan</p>
     <div class="t-info1">
-           <ul >
-           <li class="nav-item">
-            <p class="t-email">hassanhasibul5@gmail.com</a>
-            </li><li class="nav-item">
-            <p class="t-eduquali">Greatest Electric Mistri In the World</a>
-            </li><li class="nav-item">
-            <p class="t-subjects">Can Teach Anything With Fun </a>
-            </li>
-          </ul>
-         </div>
+            <ul >
+            <li class="nav-item">
+                <p class="t-email">hassanhasibul5@gmail.com</a>
+                </li><li class="nav-item">
+                <p class="t-eduquali">Greatest Electric Mistri In the World</a>
+                </li><li class="nav-item">
+                <p class="t-subjects">Can Teach Anything With Fun </a>
+                </li>
+            </ul>
+            </div>
 </div>
 </div>
 <h6 class ="t-header1">Bangla Teachers</h6>
@@ -46,16 +45,16 @@ include "includes/navbar.php";
     <li class="nav-item">
         <p class = "teachersname">Shahadat Nayeem</p>
     <div class="t-info1">
-           <ul >
-           <li class="nav-item">
+            <ul >
+            <li class="nav-item">
             <p class="t-email">shahadatnayeem23@gmail.com</a>
             </li><li class="nav-item">
             <p class="t-eduquali">Nasa's Most Young and Bright Scientist</a>
             </li><li class="nav-item">
             <p class="t-subjects">Can Teach Anything if He is willing</a>
             </li>
-          </ul>
-         </div>
+            </ul>
+            </div>
 </div>
     
 </div>
