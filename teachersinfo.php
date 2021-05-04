0d6573f894ae28d21d49ded01880dfd8469f6cad
Shahadat Nayeem <shahadatnayeem23@gmail.com>
2021-05-05 01:39:29 +0600

    conflict

--- teachersinfo.php
+++ teachersinfo.php
@@ -2,7 +2,79 @@
 include "includes/header.php";
 include "includes/navbar.php";
 ?>
-
+<div class="t-wrapper">
+<h5 class="t-header">Math teachers</h5>
+<div class="ab-container">
+        <div class="col"><div class="wrapping">
+<div class="t-container">
+    <div class="col">
+<img src="assests/safran.jpg" style="width:100%" class="t-image">
+    <div class="middle-text">
+        <div class="actual-text">Hasibul Hassan<br>Electric Mistry <br>01xxxxxxxx</div>
+    </div>
+</div>
+</div>
+</div></div>
+</div>
+<h5 class="t-header">Bangla teachers</h5>
+<div class="ab-container">
+<div class="col"><div class="wrapping">
+<div class="t-container">
+    <div class="col">
+<img src="assests/safran.jpg" style="width:100%" class="t-image">
+    <div class="middle-text">
+        <div class="actual-text">Hasibul Hassan<br>Electric Mistry <br>01xxxxxxxx</div>
+    </div>
+</div>
+</div>
+</div></div>
+        <div class="col"><div class="wrapping">
+<div class="t-container">
+    <div class="col">
+<img src="assests/shahadat.jpg" style="width:92%" class="t-image">
+    <div class="middle-text">
+        <div class="actual-text">Shahadat Nayeem<br>Nasa's Scientist <br>01xxxxxxxx</div>
+    </div>
+</div>
+</div>
+</div></div>
+</div>
+<h5 class="t-header">English teachers</h5>
+<div class="ab-container">
+<div class="col">
+<div class="wrapping">
+<div class="t-container">
+    <div class="col">
+<img src="assests/safran.jpg" style="width:100%" class="t-image">
+    <div class="middle-text">
+        <div class="actual-text">Hasibul Hassan<br>Electric Mistry <br>01xxxxxxxx</div>
+    </div>
+</div>
+</div>
+</div></div>
+<div class="col">
+<div class="wrapping">
+<div class="t-container">
+    <div class="col">
+<img src="assests/arnob.jpg" style="width:250%" class="t-image">
+    <div class="middle-text">
+        <div class="actual-text">Arnob Mallick<br>Lazy Gamer <br>01xxxxxxxx</div>
+    </div>
+</div>
+</div>
+</div></div>
+        <div class="col"><div class="wrapping">
+<div class="t-container">
+    <div class="col">
+<img src="assests/shahadat.jpg" style="width:92%" class="t-image">
+    <div class="middle-text">
+        <div class="actual-text">Shahadat Nayeem<br>Nasa's Scientist <br>01xxxxxxxx</div>
+    </div>
+</div>
+</div>
+</div></div>
+</div>
+</div>
 <?php
 include "includes/footer.php";
 ?>
