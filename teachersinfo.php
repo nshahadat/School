a6a449aa199b7922ab062167905c35b3aca222c8
Shahadat Nayeem <shahadatnayeem23@gmail.com>
2021-05-05 01:36:52 +0600

    conflict

--- teachersinfo.php
+++ teachersinfo.php
@@ -2,67 +2,6 @@
 include "includes/header.php";
 include "includes/navbar.php";
 ?>
- <h6 class=t-header1>Math teachers</h6>
-<div class="teachers-container">
-   
-<ul class="nav-links">
-    <div class="col t-image"> <img src="assests/shahadat.jpg"> 
-    <li class="nav-item">
-        <p class = "teachersname">Shahadat Nayeem</p>
-    <div class="t-info1">
-           <ul >
-           <li class="nav-item">
-            <p class="t-email">shahadatnayeem23@gmail.com</a>
-            </li><li class="nav-item">
-            <p class="t-eduquali">Nasa's Most Young and Bright Scientist</a>
-            </li><li class="nav-item">
-            <p class="t-subjects">Can Teach Anything if He is willing</a>
-            </li>
-            </li>
-          </ul>
-         </div>
-             
-</div>
-    
-
-    <div class="col t-image"><img src="assests/safran.jpg">
-    <li class="nav-item">
-    <p class = "teachersname">Hasibul Hassan</p>
-    <div class="t-info1">
-           <ul >
-           <li class="nav-item">
-            <p class="t-email">hassanhasibul5@gmail.com</a>
-            </li><li class="nav-item">
-            <p class="t-eduquali">Greatest Electric Mistri In the World</a>
-            </li><li class="nav-item">
-            <p class="t-subjects">Can Teach Anything With Fun </a>
-            </li>
-            </li>
-          </ul>
-         </div>
-         </ul>
-</div>
-</div>
-<h6 class ="t-header1">Bangla Teachers</h6>
-<div class="teachers-container">
-<ul class="nav-links">
-    <div class="col t-image"> <img src="assests/shahadat.jpg"> 
-    <li class="nav-item">
-        <p class = "teachersname">Shahadat Nayeem</p>
-    <div class="t-info1">
-           <ul >
-           <li class="nav-item">
-            <p class="t-email">shahadatnayeem23@gmail.com</a>
-            </li><li class="nav-item">
-            <p class="t-eduquali">Nasa's Most Young and Bright Scientist</a>
-            </li><li class="nav-item">
-            <p class="t-subjects">Can Teach Anything if He is willing</a>
-            </li>
-            </li>
-          </ul>
-         </div>
-         </ul>
-</div></div> 
 
 <?php
 include "includes/footer.php";
