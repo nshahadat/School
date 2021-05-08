ed182569e9cc848b9ce1f17fa261dd5d5aae055d
Arnob <arnobm97@gmail.com>
2021-05-08 19:16:42 +0600

    depts added

--- depts.php
+++ depts.php
@@ -4,51 +4,12 @@ include "includes/header.php";
 include "includes/navbar.php";
 ?>
  
- <h1 class="table-name">Bangla Version</h1>
-<div class="table-conainter">
-    <div class="row">
-        <div class="col">
-           <table class="table">
-                    <tr class="table-header">
-                        <th>Science</th>
-                        <th>Commerce</th>
-                        
-                    </tr>
-                
-                        <tr>
-                            <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
-                            <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
-                        </tr>
-                        <tr>
-                            <td>Classes:10<br>Student Capacity:80<br>Current Students:70</td>
-                            <td>Classes:10<br>Student Capacity:80<br>Current Students:70</td>
-                        </tr>
-                
-                    <tr class="table-header">
-                        <th ><p class="others">Others</p><th ></th></th>
-                        
-                    </tr>
-                        <tr>
-                            <td>Classes:1<br>Student Capacity:80<br>Current Students:70</td>
-                            <td>Classes:2<br>Student Capacity:80<br>Current Students:70</td>
-                        </tr>
-                        <tr>
-                            <td>Classes:3<br>Student Capacity:80<br>Current Students:70</td>
-                            <td>Classes:4<br>Student Capacity:80<br>Current Students:70</td>
-                        </tr>
-                        <tr>
-                            <td>Classes:5<br>Student Capacity:80<br>Current Students:70</td>
-                            <td>Classes:6<br>Student Capacity:80<br>Current Students:70</td>
-                        </tr>
-                        <tr>
-                            <td>Classes:7<br>Student Capacity:80<br>Current Students:70</td>
-                            <td>Classes:8<br>Student Capacity:80<br>Current Students:70</td>
-                        </tr>
-                </table>
-        </div>
-        <div class="col">
-            <h1 class="table-name1">English Version</h1>
-                    <table class="table1">
+ 
+<div class="table-containter"> 
+        <div class="row">
+            <div class="col">
+            <h1 class="table-name">Bangla Version</h1>
+            <table class="table">
                         <tr class="table-header">
                             <th>Science</th>
                             <th>Commerce</th>
@@ -84,9 +45,50 @@ include "includes/navbar.php";
                                 <td>Classes:7<br>Student Capacity:80<br>Current Students:70</td>
                                 <td>Classes:8<br>Student Capacity:80<br>Current Students:70</td>
                             </tr>
-                           
-                        </table>
-        </div>   
+                    </table>
+            </div>
+            
+            <div class="col">
+            <h1 class="table-name1">English Version</h1>
+                        <table class="table">
+                            <tr class="table-header">
+                                <th>Science</th>
+                                <th>Commerce</th>
+                                
+                            </tr>
+                        
+                                <tr>
+                                    <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
+                                    <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
+                                </tr>
+                                <tr>
+                                    <td>Classes:10<br>Student Capacity:80<br>Current Students:70</td>
+                                    <td>Classes:10<br>Student Capacity:80<br>Current Students:70</td>
+                                </tr>
+                        
+                            <tr class="table-header">
+                                <th ><p class="others">Others</p><th ></th></th>
+                                
+                            </tr>
+                                <tr>
+                                    <td>Classes:1<br>Student Capacity:80<br>Current Students:70</td>
+                                    <td>Classes:2<br>Student Capacity:80<br>Current Students:70</td>
+                                </tr>
+                                <tr>
+                                    <td>Classes:3<br>Student Capacity:80<br>Current Students:70</td>
+                                    <td>Classes:4<br>Student Capacity:80<br>Current Students:70</td>
+                                </tr>
+                                <tr>
+                                    <td>Classes:5<br>Student Capacity:80<br>Current Students:70</td>
+                                    <td>Classes:6<br>Student Capacity:80<br>Current Students:70</td>
+                                </tr>
+                                <tr>
+                                    <td>Classes:7<br>Student Capacity:80<br>Current Students:70</td>
+                                    <td>Classes:8<br>Student Capacity:80<br>Current Students:70</td>
+                                </tr>
+                            
+                            </table>
+            </div>   
 </div>
 	
 <?php
