f82e11d1c211d737e27be19364a0d453f6729ca4
Shahadat Nayeem <shahadatnayeem23@gmail.com>
2021-05-11 05:10:08 +0600

    safran code added html

--- rules.php
+++ rules.php
@@ -3,6 +3,52 @@ include "includes/header.php";
 include "includes/navbar.php";
 ?>
 
+<div class="contaion">
+	<div class="box">
+		<h3>SCHOOL RULES:-</h3>
+		<p>
+			<br />
+			1. Students should be polite and courteous to staff, each other and
+			visitors. <br />
+			2. Students are expected to be punctual for school and lessons. <br />
+			3. Full school uniform is to be worn (See Uniform page). <br />
+			4. Smoking is strictly forbidden – both at school and while travelling to
+			and from school. <br />
+			5. No non-school uniform (Coats, jumpers, hoodies,etc) are to be worn in
+			the school buildings. <br />
+			<br />
+		</p>
+	</div>
+	<div class="box">
+		<h3>Classroom rules:-</h3>
+		<p>
+			<br />
+			1. Be on time at the beginning of the day and after lunch or recess
+			breaks. <br />
+			2. Come prepared with supplies and completed homework. <br />
+			3. Be kind, polite, and courteous to others. <br />
+			4. Keep your hands and feet to yourself. <br />
+			5. Be respectful of classmates, teachers, and property. <br /><br />
+		</p>
+	</div>
+	<div class="box">
+		<h3>LABORATORY RULES:-</h3>
+		<p>
+			<br />
+			1. Students should be polite and courteous to staff, each other and
+			visitors. <br />
+			2. Students are expected to be punctual for school and lessons. <br />
+			3. Full school uniform is to be worn (See Uniform page). <br />
+			4. Smoking is strictly forbidden – both at school and while travelling to
+			and from school. <br />
+			5. No non-school uniform (Coats, jumpers, hoodies,etc) are to be worn in
+			the school buildings. <br />
+			<br />
+		</p>
+	</div>
+</div>
+
+
 <?php
 include "includes/footer.php";
 ?>
--- vision.php
+++ vision.php
@@ -3,6 +3,60 @@ include "includes/header.php";
 include "includes/navbar.php";
 ?>
 
+<div class="v">
+	<div class="vbox">
+		<h3>Mission</h3>
+
+		<p>
+			The New School prepares students to understand, contribute to, <br />
+			and succeed in a rapidly changing society, thus making the world a <br />
+			better and more just place. We will ensure that our students <br />
+			develop both the skills that a sound education provides and the
+			competencies essential <br />
+			for success and leadership in the emerging creative economy. We will also
+			<br />
+			lead in generating practical and theoretical knowledge that enables people
+			to better understand our world and improve <br />
+			conditions for local and global communities. <br />
+			<br />
+		</p>
+	</div>
+
+	<div class="vbox">
+		<h3>Vision</h3>
+		<p>
+			We are and will be a university where design and social research drive
+			approaches to studying issues of our time, such as democracy,
+			urbanization, technological change, economic empowerment, sustainability,
+			<br />
+			migration, and globalization. We will be the preeminent intellectual and
+			<br />
+			creative center for effective engagement in a world that increasingly
+			demands better-designed objects, <br />
+			communication, systems, and organizations to meet social needs. <br />
+			<br />
+			Our vision aligns with shifts in the global economy, society, and
+			environment, which animates our mission and our values: <br />
+			<br />
+
+			#Creativity, innovation, and a desire to challenge the status quo will
+			affect what and how we teach and the intellectual ambitions of the
+			university itself. <br />
+			#Social engagement should orient students' academic experiences to help
+			them become critically engaged citizens, dedicated to solving problems and
+			contributing to the public good. <br />
+			#The New School must embrace these principles and innovate to address
+			shifts in the global economy, <br />
+			society, and environment that require individuals to grapple with complex
+			problems, <br />
+			pursue more fluid and flexible career pathways, and collaboratively create
+			change. <br />
+			<br />
+		</p>
+	</div>
+</div>
+
+
 <?php
 include "includes/footer.php";
 ?>
