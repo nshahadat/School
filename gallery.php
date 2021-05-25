0732bec42084b83648436f33c37bdbe5af34b280
Arnob <arnobm97@gmail.com>
2021-05-25 15:11:02 +0600

    gallery added

--- gallery.php
+++ gallery.php
@@ -2,17 +2,41 @@
 include "includes/header.php";
 include "includes/navbar.php";
 ?>
-<div class="g-container">
-    <h1 class="g-name1">g1</h1>
-        <img class="g1-img1" src="assests/1.jpg"> 
-        <img class="g1-img2" src="assests/1.jpg">    
-        <img class="g1-img3" src="assests/1.jpg">    
-        <img class="g1-img4" src="assests/1.jpg">  
-    <h1 class="g-name2">g2</h1>
-        <img class="g2-img1" src="assests/1.jpg"> 
-        <img class="g2-img2" src="assests/1.jpg">    
-        <img class="g2-img3" src="assests/1.jpg">    
-        <img class="g2-img4" src="assests/1.jpg">                   
+<h1 class="g-name">g1</h1>
+<div class="gallery">
+    <div class="img-container">
+            <img src="assests/Example.jpg"alt="Mountain"data-orginal="assests/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="assests/Example.jpg"alt="Mountain"data-orginal="assests/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="assests/Example.jpg"alt="Mountain"data-orginal="assests/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="assests/Example.jpg"alt="Mountain"data-orginal="assests/Examplebig.jpg">
+    </div> 
+
+</div>
+<h1 class="g-name">g2</h1>
+<div class="gallery">
+        <div class="img-container">
+            <img src="assests/Example.jpg"alt="Mountain"data-orginal="assests/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="assests/Example.jpg"alt="Mountain"data-orginal="assests/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="assests/Example.jpg"alt="Mountain"data-orginal="assests/Examplebig.jpg">
+    </div> 
+    <div class="img-container">
+            <img src="assests/Example.jpg"alt="Mountain"data-orginal="assests/Examplebig.jpg">
+    </div> 
+    </div>
+</div>
+<div class="modal">
+    <img src="assests/Examplebig.jpg" alt=""class="full-img">
+    <p class="caption">mountain</p>
 </div>
 <?php
 include "includes/footer.php";
--- js/main.js
+++ js/main.js
@@ -1,42 +1,60 @@
 // Image SLider START
-let sliderImages = document.querySelectorAll(".slide"),
-	arrowRight = document.querySelector("#arrow-right"),
-	arrowLeft = document.querySelector("#arrow-left"),
-	current = 0;
+// let sliderImages = document.querySelectorAll(".slide"),
+// 	arrowRight = document.querySelector("#arrow-right"),
+// 	arrowLeft = document.querySelector("#arrow-left"),
+// 	current = 0;
 
-function reset() {
-	for (let i = 0; i < sliderImages.length; i++) {
-		sliderImages[i].style.display = "none";
-	}
-}
+// function reset() {
+// 	for (let i = 0; i < sliderImages.length; i++) {
+// 		sliderImages[i].style.display = "none";
+// 	}
+// }
 
-function startSlide() {
-	reset();
-	sliderImages[0].style.display = "block";
-}
+// function startSlide() {
+// 	reset();
+// 	sliderImages[0].style.display = "block";
+// }
 
-function slideLeft() {
-	reset();
-	sliderImages[current - 1].style.display = "block";
-	current--;
-}
+// function slideLeft() {
+// 	reset();
+// 	sliderImages[current - 1].style.display = "block";
+// 	current--;
+// }
 
-function slideRight() {
-	reset();
-	sliderImages[current + 1].style.display = "block";
-	current++;
-}
-arrowLeft.addEventListener("click", function () {
-	if (current === 0) {
-		current = sliderImages.length;
-	}
-	slideLeft();
-});
-arrowRight.addEventListener("click", function () {
-	if (current === sliderImages.length - 1) {
-		current = -1;
-	}
-	slideRight();
-});
-startSlide();
+// function slideRight() {
+// 	reset();
+// 	sliderImages[current + 1].style.display = "block";
+// 	current++;
+// }
+// arrowLeft.addEventListener("click", function () {
+// 	if (current === 0) {
+// 		current = sliderImages.length;
+// 	}
+// 	slideLeft();
+// });
+// arrowRight.addEventListener("click", function () {
+// 	if (current === sliderImages.length - 1) {
+// 		current = -1;
+// 	}
+// 	slideRight();
+// });
+// startSlide();
 // Image Slider END
+//gallery start
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img");
+const orginal = document.querySelector(".full-img");
+const imgText = document.querySelector(".caption");
+
+previews.forEach((preview) => {
+	preview.addEventListener("click", function () {
+		modal.classList.add("open");
+		orginal.classList.add("open");
+	});
+});
+modal.addEventListener("click",(e)=>{
+if(e.target.classList.contains('modal')){
+	modal.classList.remove("open");
+	orginal.classList.add("open");
+}
+});
