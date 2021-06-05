e226f5ee8192485ffcf0b5e191930694d5a85fae
Shahadat Nayeem <shahadatnayeem23@gmail.com>
2021-06-06 00:57:42 +0600

    login php added

--- admin.php
+++ admin.php
@@ -1,8 +1,10 @@
 <?php
 include "includes/header.php";
 include "includes/navbar.php";
-?><div class="admin-container">
-     <p class="admin-header"> Admin Login</p>
+include "includes/dbconfig.php";
+?>
+<div class="admin-container">
+    <p class="admin-header"> Admin Login</p>
     <div class="loginbox">
         <form class="login" method="POST">
             <input type="text" placeholder="Username"name="uname" class="uname" />
@@ -11,6 +13,24 @@ include "includes/navbar.php";
         </form>
     </div>
 </div>    
+
+<?php
+if (isset($_POST['submit'])) {
+    $name = $_POST['uname'];
+    $pwd = $_POST['pass'];
+
+    $sql = "SELECT * FROM $table WHERE name = '$name' AND pwd = '$pwd'";
+
+    $result  = mysqli_query($mysqli, $sql) or die(mysqli_error());
+    $numrows = mysqli_num_rows($result);
+
+    if ($numrows == 0) {
+        echo "<script>alert('Wrong username or password')</script>";
+    } else {
+        header("Location: depts.php");
+    }
+}
+?>
 <?php
 include "includes/footer.php";
 ?>
---
+++ includes/dbconfig.php
@@ -0,0 +1,3 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', '', 'school') or die($mysqli->connect_error);
+$table  = 'admin';
