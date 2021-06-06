1b9f0fc578ee47b4c58e73c8d6d25e450ac695a0
Arnob <arnobm97@gmail.com>
2021-06-06 23:26:55 +0600

    submit button added

--- admin-c-notice.php
+++ admin-c-notice.php
@@ -3,12 +3,21 @@ include "includes/header.php";
 include "includes/navbar.php";
 ?><div class="control-notice-container">
     <p class="notice-header1"> Upload Your Notices Here-</p>
-    <form action="post">
-    <input type="file" value="Upload" class="notice-upload"name="upload" accept="application/pdf, application/vnd.ms-excel" />
-    </form>
+    <table class="notice-table1">
+                       <tr>
+                                <td class="uploaded_notices">
+                                    <form method="post">
+                                    <input type="file" value="Upload" class="notice-upload"name="upload" accept="application/pdf, application/vnd.ms-excel" />
+                                    </form>
+                                </td>
+                                <td><input type="submit" value="submit" class="submit-button"name="delete"/></td>
+                        </tr>
+                            <tr>  
+                            </tr>
+    </table>
     <p class="notice-header2"> Your Uploaded Notices</p>
-    <form action="post">
-    <table class="notice-table">
+    <form method="post">
+    <table class="notice-table2">
                        <tr>
                                 <td class="uploaded_notices">First notice uploaded here</td>
                                 <td><input type="submit" value="Delete" class="delete-button"name="delete"/></td>
