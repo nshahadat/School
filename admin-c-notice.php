babe4c99fc03cc1d6f70d19bfcb921c6b94ddd4e
Arnob <arnobm97@gmail.com>
2021-07-03 23:52:11 +0600

    notice page fixed

--- admin-c-notice.php
+++ admin-c-notice.php
@@ -8,9 +8,9 @@ include "includes/navbar.php";
                                 <td class="uploaded_notices">
                                     <form method="post">
                                     <input type="file" value="Upload" class="notice-upload"name="upload" accept="application/pdf, application/vnd.ms-excel" />
-                                    </form>
                                 </td>
-                                <td><input type="submit" value="submit" class="submit-button"name="delete"/></td>
+                                <td><input type="submit" value="submit" class="submit-button"name="submit"/></td>
+                                </form>
                         </tr>
                             <tr>  
                             </tr>
@@ -22,8 +22,11 @@ include "includes/navbar.php";
                                 <td class="uploaded_notices">First notice uploaded here</td>
                                 <td><input type="submit" value="Delete" class="delete-button"name="delete"/></td>
                             </tr>
-                            <tr>  
+                            <tr> 
+                            <td class="uploaded_notices">First notice uploaded here</td>
+                                <td><input type="submit" value="Delete" class="delete-button"name="delete"/></td> 
                             </tr>
+                            
     </table>
     </form>
 </div>
--- admin-c-results.php
+++ admin-c-results.php
@@ -2,8 +2,21 @@
 include "includes/header.php";
 include "includes/navbar.php";
 ?>
-results control page 
-
+<div class="control-notice-container">
+    <p class="notice-header1"> Upload The Result Here-</p>
+    <table class="notice-table1">
+                       <tr>
+                                <td class="uploaded_notices">
+                                    <form method="post">
+                                    <input type="file" value="Upload" class="notice-upload"name="upload" accept="application/pdf, application/vnd.ms-excel" />
+                                </td>
+                                <td><input type="submit" value="submit" class="submit-button"name="submit"/></td>
+                                </form>
+                        </tr>
+                            <tr>  
+                            </tr>
+    </table>
+</div>
 <?php
 include "includes/footer.php";
 ?>
